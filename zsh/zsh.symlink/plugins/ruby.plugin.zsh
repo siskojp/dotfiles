a5f678c6f0c41203fcca360d7688846949ad3ebe
gcoguiec <mail@gcoguiec.com>
2013-03-28 17:18:09 -0400

    Change the default serve alias from thin to puma.

--- zsh/zsh.symlink/plugins/ruby.plugin.zsh
+++ zsh/zsh.symlink/plugins/ruby.plugin.zsh
@@ -14,5 +14,5 @@ alias be='bundle exec'
 alias bl="bundle list"
 alias bp="bundle package"
 alias annotate="annotate --position before"
-alias st="bundle exec rails server thin --binding 0.0.0.0 --port"
-alias ss="bundle exec shotgun --server thin --host 0.0.0.0 --port"
+alias st="bundle exec rails server puma --binding 0.0.0.0 --port"
+alias ss="bundle exec shotgun --server puma --host 0.0.0.0 --port"
