f7dd9c4dc8a4c17b11a142cf2b0e09c33710e459
gcoguiec <mail@gcoguiec.com>
2014-01-15 22:07:38 -0500

    Update python packages and shell env.

--- zsh/zsh.symlink/plugins/python.plugin.zsh
+++ zsh/zsh.symlink/plugins/python.plugin.zsh
@@ -2,12 +2,7 @@
 export PATH="$(brew --prefix python)/bin":$PATH
 export PATH=/usr/local/share/python:$PATH
 export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
-export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
-if [[ -r /usr/local/share/python/virtualenvwrapper.sh ]]; then
-    source /usr/local/share/python/virtualenvwrapper.sh
-else
-    echo "WARNING: Can't find virtualenvwrapper.sh"
-fi
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
